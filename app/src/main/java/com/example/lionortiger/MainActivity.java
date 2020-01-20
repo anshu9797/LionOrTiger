d6539a2fa1b41b1d2a90374586787926205b8d4a
anshu97 <anshumanghosh56@gmail.com>
2020-01-20 15:39:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.lionortiger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    enum Player {
+        ONE, TWO, No
+    }
+    Player currentPlayer = Player.ONE;
+    Player[] playerChoices = new Player[9];
+    int [][] winnerRowsColumns = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    private boolean gameOver = false;
+    private Button btnReset;
+    private GridLayout gridLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playerChoices[0] = Player.No;
+        playerChoices[1] = Player.No;
+        playerChoices[2] = Player.No;
+        playerChoices[3] = Player.No;
+        playerChoices[4] = Player.No;
+        playerChoices[5] = Player.No;
+        playerChoices[6] = Player.No;
+        playerChoices[7] = Player.No;
+        playerChoices[8] = Player.No;
+
+        btnReset = findViewById(R.id.btnReset);
+        gridLayout = findViewById(R.id.gridLayout);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                resetTheGame();
+
+            }
+        });
+
+    }
+
+    public void imageViewIsTapped(View imageView){
+
+        ImageView tappedImageView = (ImageView) imageView;
+
+        int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+
+        if(playerChoices[tiTag] == Player.No && gameOver == false) {
+            tappedImageView.setTranslationX(-2000);
+            playerChoices[tiTag] = currentPlayer;
+            if (currentPlayer == Player.ONE) {
+                tappedImageView.setImageResource(R.drawable.lion);
+                currentPlayer = Player.TWO;
+            } else if (currentPlayer == Player.TWO) {
+                tappedImageView.setImageResource(R.drawable.tiger);
+                currentPlayer = Player.ONE;
+            }
+            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+            Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
+
+            for (int[] winnerColumns : winnerRowsColumns) {
+                if (playerChoices[winnerColumns[0]] == playerChoices[winnerColumns[1]]
+                        && playerChoices[winnerColumns[1]] == playerChoices[winnerColumns[2]]
+                        && playerChoices[winnerColumns[0]] != Player.No) {
+                    btnReset.setVisibility(View.VISIBLE);
+                    gameOver = true;
+                    if (currentPlayer == Player.ONE) {
+                        Toast.makeText(this, "Player two is the winner", Toast.LENGTH_LONG).show();
+                    } else if (currentPlayer == Player.TWO) {
+                        Toast.makeText(this, "Player one is the winner", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private void resetTheGame(){
+
+        for (int index = 0; index < gridLayout.getChildCount(); index++){
+
+            ImageView imageView = (ImageView) gridLayout.getChildAt(index);
+            imageView.setImageDrawable(null);
+            imageView.setAlpha(0.2f);
+
+        }
+
+        currentPlayer = Player.ONE;
+
+        playerChoices[0] = Player.No;
+        playerChoices[1] = Player.No;
+        playerChoices[2] = Player.No;
+        playerChoices[3] = Player.No;
+        playerChoices[4] = Player.No;
+        playerChoices[5] = Player.No;
+        playerChoices[6] = Player.No;
+        playerChoices[7] = Player.No;
+        playerChoices[8] = Player.No;
+
+        gameOver = false;
+        btnReset.setVisibility(View.INVISIBLE);
+
+    }
+
+}
